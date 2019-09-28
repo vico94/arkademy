1f72b525588ba7785d5dc5e83974d0eb081ddab7
vico94 <55688545+vico94@users.noreply.github.com>
2019-09-28 21:21:24 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,23 @@
+function biodataku () {
+  const data = {
+  name :"Vicochian Panca",
+  age : 25,
+  address : "Kav. Polri Blok C/755",
+  hobbies : ["Kuliner","Gadget","Music"],
+  is_married : false,
+  list_school : [
+    {name : "Institut Pertanian Bogor",
+    year_in : 2011, 
+    year_out : 2016,
+    major : "Teknologi Pangan",
+    }
+  ],
+  skills : [
+    {skill_name : "crurch organist",
+    level : "advanced",
+    }
+  ],
+  interest_in_coding : true
+}
+  return data;
+}
